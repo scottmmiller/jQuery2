558b1548c15fb49885ebd5d1dc04117d10522526
skootdiggity <smmiller.work@gmail.com>
2015-01-15 14:26:52 -0700

    advanceTask

--- scripts/app.js
+++ scripts/app.js
@@ -22,6 +22,24 @@ $(document).ready(function() {
 		$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
 	};
 
+	var advanceTask = function(task) {
+	    var modified = task.innerText.trim()
+	    for (var i = 0; i < listo.length; i++) {
+	        if (listo[i].task === modified) {
+	            if (listo[i].id === 'new') {
+	                listo[i].id = 'inProgress';
+	            } else if (listo[i].id === 'inProgress') {
+	                listo[i].id = 'archived';
+	            } else {
+	                listo.splice(i, 1);
+	            };
+
+	            break;
+	        };
+	    };
+	    task.remove();
+	};
+
 	$('saveNewItem').on('click', function (e) {
 		e.preventDefault();
 		var task = $('#newItemInput').val().trim();
@@ -39,7 +57,22 @@ $(document).ready(function() {
 		$('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
 	});
 
+	$(document).on('click', '#item', function (e) {
+		e.preventDefault();
+		var task = this;
+		advanceTask(task);
+		this.id = 'inProgress';
+		$('#currentList').append(this.outerHTML);
+	});
 
+	$(document).on('click', '#inProgress', function(e) {
+		e.preventDefault();
+		var task = this;
+		task.id = "archived";
+		var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+		advanceTask(task);
+		$('#archivedList').append(changeIcon);
+	});
 
 
 
